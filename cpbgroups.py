39b8cec921b2bc942098ec6baa5b878749393811
Machairodus <35767006+machairod@users.noreply.github.com>
2021-06-23 15:33:22 +0300

    Update cpbgroups.py

--- cpbgroups.py
+++ cpbgroups.py
@@ -1,2 +1,2 @@
-channel ='@zmeeust_python'
-groups = {'spb_cpb': 1621355136, 'msk_cpb': 1620726445, 'cpb_vl':1621324449,'cpb_corp':1619350120,'cpb_krasnodar':1620751301,'irk_cpb': 1616710094,'krim_cpb': 1620726432, '189820518': 1621019361,'102325800': 1620726432}
+channel ='@cpbchannel'
+groups ={'spb_cpb': 1624442883, 'msk_cpb': 1623491120, 'cpb_vl':1624188737,'cpb_corp':1622739657,'cpb_krasnodar':1620751301,'irk_cpb': 1616710094,'krim_cpb': 1624109160, '189820518': 1624295409,'102325800': 1624109160}
